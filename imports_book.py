890fea0476eb80f875aa3a7db555581d64d478ed
Andrew Alderfer <andrew@drewalderfer.com>
2022-10-31 03:04:36 -0400

    three

---
+++ imports_book.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.linear_model import LinearRegression
+from statsmodels.api import stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+
